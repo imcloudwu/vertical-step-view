0021f426db8b4aa54f03dd1a8d8ada4c512d1bc3
Cloud <imcloudwu@gmail.com>
2022-01-20 18:05:46 +0800

    Fix dependency issue

--- app/src/main/java/com/imcloudwu/android/sample/MainActivity.kt
+++ app/src/main/java/com/imcloudwu/android/sample/MainActivity.kt
@@ -1,37 +1,11 @@
 package com.imcloudwu.android.sample
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.imcloudwu.android.component.VerticalStepView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val verticalStepView = findViewById<VerticalStepView>(R.id.verticalStepView)
-        val resetBtn = findViewById<Button>(R.id.reset_btn)
-        val nextBtn = findViewById<Button>(R.id.next_btn)
-
-        val data = arrayListOf<String>()
-
-        repeat(100) {
-            data.add("Test step $it")
-        }
-
-        verticalStepView.setSteps(data)
-
-        var index = -1
-
-        resetBtn.setOnClickListener {
-            index = -1
-            verticalStepView.move(index)
-        }
-
-        nextBtn.setOnClickListener {
-            index += 20
-            verticalStepView.move(index)
-        }
     }
 }
